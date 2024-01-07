bd5f432e0745a536eb5207925011827bb24db115
Sena Bi <94117345+senabibi@users.noreply.github.com>
2024-01-07 20:02:10 +0300

    Create 100_000_pairs.py

---
+++ BigO/Interview_Questions/100_000_pairs.py
@@ -0,0 +1,6 @@
+def print_unorderd_pairs(arrayA,arrayB ):
+    for i in range(len(arrayA)):
+        for j in range(len(arrayB)):
+            for k in range(0,100000):
+                print(arrayA[i],arrayB[j])
+                
