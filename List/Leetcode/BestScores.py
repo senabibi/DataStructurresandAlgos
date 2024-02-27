a18f662985201c84a8c991c472f3f5f62303457b
Sena Bi <94117345+senabibi@users.noreply.github.com>
2024-02-27 04:31:31 +0300

    Create BestScores.py

---
+++ List/Leetcode/BestScores.py
@@ -0,0 +1,9 @@
+
+def first_second(my_list):
+    my_list.sort()
+    my_list.reverse()
+    return print(my_list[0],myList[1])
+
+
+myList = [84,85,86,87,85,90,85,83,23,45,84,1,2,0]
+(first_second(myList))
