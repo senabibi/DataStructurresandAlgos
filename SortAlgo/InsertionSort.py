af8ee367fc2e85c3dc690bfbb73d711caf519a2b
Sena Bi <94117345+senabibi@users.noreply.github.com>
2024-04-11 23:26:14 +0300

    Create InsertionSort.py

---
+++ SortAlgo/InsertionSort.py
@@ -0,0 +1,9 @@
+def insertion_sort(customList):
+    for i in range(1, len(customList)):
+        key = customList[i]
+        j = i - 1
+        while j >= 0 and key < customList[j]:
+            customList[j + 1] = customList[j]
+            j -= 1
+        customList[j + 1] = key
+    return customList
