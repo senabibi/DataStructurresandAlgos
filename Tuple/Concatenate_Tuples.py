af525d6d5f23fbd99ab8554b25b3ad576caeb98a
Sena Bi <94117345+senabibi@users.noreply.github.com>
2024-05-16 21:42:45 +0300

    Create Concatenate_Tuples.py

---
+++ Tuple/Concatenate_Tuples.py
@@ -0,0 +1,10 @@
+
+def concatenate_strings(input_tuple):
+    return ' '.join(input_tuple)
+
+    
+    
+    
+input_tuple = ('Hello', 'World', 'from', 'Python')
+output_string = concatenate_strings(input_tuple)
+print(output_string)  # Expected output: 'Hello World from Python'
