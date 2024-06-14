247c60cf30bd7e70be07e63287f3dab4ca87927a
Sena Bi <94117345+senabibi@users.noreply.github.com>
2024-06-14 23:33:53 +0300

    Create Program.cs

---
+++ Razor/MyPrograms/Program.cs
@@ -0,0 +1,12 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace My_Programs{
+    class Program{
+        static void Main(){
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+
+}
